0dd9c7e4de62cf6831adc322867ab7b1cfd691bb
Leto <m.timoshin88@gmail.com>
2026-03-10 22:37:00 +0300

    feat: container summary

--- container_parser_models.go
+++ container_parser_models.go
@@ -150,8 +150,8 @@ type ContainerSummary struct {
 	MemPercent     float64           `json:"mem_percent"`
 	NetworkRx      string            `json:"network_rx"`
 	NetworkTx      string            `json:"network_tx"`
-	NetworkRxPS    string            `json:"network_rx_ps"`
-	NetworkTxPS    string            `json:"network_tx_ps"`
+	NetworkRxPS    uint64            `json:"network_rx_ps"`
+	NetworkTxPS    uint64            `json:"network_tx_ps"`
 	IsCompose      bool              `json:"is_compose"`
 	ComposeProject string            `json:"compose_project,omitempty"`
 	ComposeService string            `json:"compose_service,omitempty"`
